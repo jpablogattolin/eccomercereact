428c6c9ab86a0e339164e1b9945df68d87d0dfc6
Juan Pablo <underkron@hotmail.com>
2023-11-23 19:49:09 -0300

    arreglos

--- src/App.js
+++ src/App.js
@@ -1,40 +1,36 @@
-import React from 'react';
-import NavBar from './components/NavBar';
-import ItemListContainer from './components/ItemListCointainer';
-import DetallesProducto from './components/DetallesProducto';
-import ItemDetailContainer from './components/ItemDetailContainer';
-import { BrowserRouter as Routes, Route, Switch } from 'react-router-dom';
+import React from "react";
+import NavBar from "./components/NavBar";
+import ItemListContainer from "./components/ItemListCointainer";
+import DetallesProducto from "./components/DetallesProducto";
+import ItemDetailContainer from "./components/ItemDetailContainer";
+import {
+  BrowserRouter as Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 //import './styles.css';
-
 
 const App = () => {
   return (
+    <>
+      <BrowserRouter>
+        <Navbar />
 
-    <><Routes>
-    <div>
-      
-      <Navbar />     
-     
-        <Route path="/" exact component={ItemListContainer} />
+        <Routes>
+          <Route path="/" element={ItemListContainer} />
 
+          <Route path="/category/:id" element={ItemListContainer} />
+
+          <Route path="/item/:id" element={ItemDetailContainer} />
+        </Routes>
+      </BrowserRouter>
+
+      <div className="app">
         
-        <Route path="/category/:id" component={ItemListContainer} />
-
-        
-        <Route path="/item/:id" component={ItemDetailContainer} />
-
-        
-        <Route component={ItemListContainer} />
-      
-    </div>
-  </Routes>
-
-    
-    <div className="app">
-        <NavBar />
         <ItemListContainer greeting="¡Bienvenido a nuestra tienda en línea!" />
-      </div></>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
-
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
